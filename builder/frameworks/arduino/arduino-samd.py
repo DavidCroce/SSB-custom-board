a714abfe92f39ff8a74ed0bb0ffa524445d03386
DavidCroce <126100052+DavidCroce@users.noreply.github.com>
2024-10-29 23:29:29 +0100

    Update arduino-samd.py

--- builder/frameworks/arduino/arduino-samd.py
+++ builder/frameworks/arduino/arduino-samd.py
@@ -32,7 +32,7 @@ board = env.BoardConfig()
 
 VENDOR_CORE = board.get("build.core", "").lower()
 
-framework_package = "SSB-custom-board"
+framework_package = "framework-arduino-samd"
 if VENDOR_CORE != "arduino":
     framework_package += "-%s" % VENDOR_CORE
 FRAMEWORK_DIR = platform.get_package_dir(framework_package)
